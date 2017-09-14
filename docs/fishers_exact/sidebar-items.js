22fdc3d24ff8b451284b3720488d54f0cc914609
Chris Pearce <cpearce@mozilla.com>
2017-09-14 21:55:49 +1200

    Add docs.

---
+++ docs/fishers_exact/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["TestTails","Passed to the fishers_exact function to express whether to perform a one or two tailed statistical test."]],"fn":[["fishers_exact","Computes the Fisher's exact test to determine if there are nonrandom associations between two categorical variables, in a two by two contingency table."]]});
---
+++ docs/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fishers_exact"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"fishers_exact/enum.TestTails.html\" title=\"enum fishers_exact::TestTails\">TestTails</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
